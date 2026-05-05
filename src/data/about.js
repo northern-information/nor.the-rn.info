2165075df73db74eb35b6b8eed786af4f498bffd
Tyler Etters <tyler@etters.co>
2026-05-04 17:13:04 -0700

    hide some images

--- src/data/about.js
+++ src/data/about.js
@@ -1,9 +1,9 @@
 export default [
-  {
-    url: '/images/celestial-construction.jpg',
-    title: 'Celestial Construction',
-    year: '02025',
-  },
+  // {
+  //   url: '/images/celestial-construction.jpg',
+  //   title: 'Celestial Construction',
+  //   year: '02025',
+  // },
   {
     url: '/images/goth-pro-shops.jpg',
     title: 'Goth Pro Shops',
@@ -19,11 +19,11 @@ export default [
     title: 'Abstraction',
     year: '02024',
   },
-  {
-    url: '/images/terminus-tyler-1240x.jpg',
-    title: 'Terminus',
-    year: '02024',
-  },
+  // {
+  //   url: '/images/terminus-tyler-1240x.jpg',
+  //   title: 'Terminus',
+  //   year: '02024',
+  // },
   {
     url: '/images/stuxnet.jpg',
     title: 'STUXNET',
@@ -49,88 +49,88 @@ export default [
     title: 'Tyler Etters illustration by Gage Lindsten',
     year: '02017',
   },
-  {
-    url: '/images/the-burlington.jpg',
-    title: 'Tyler Etters, Arthur Zdrinc, and Adam Moore at The Burlington',
-    year: '02015',
-  },
-  {
-    url: '/images/tyler-etters-ophelia.jpg',
-    title: 'Tyler Etters © Sage Etters',
-    year: '02015',
-  },
-  {
-    url: '/images/tyler-etters-adam-moore-hostel-earphoria.jpg',
-    title: 'Tyler Etters and Adam Moore at Hostel Earphoria © Riley Cavanaugh',
-    year: '02015',
-  },
-  {
-    url: '/images/tyler-etters-adam-moore-fox-river.jpg',
-    title: 'Tyler Etters and Adam Moore on the Fox River',
-    year: '02015',
-  },
+  // {
+  //   url: '/images/the-burlington.jpg',
+  //   title: 'Tyler Etters, Arthur Zdrinc, and Adam Moore at The Burlington',
+  //   year: '02015',
+  // },
+  // {
+  //   url: '/images/tyler-etters-ophelia.jpg',
+  //   title: 'Tyler Etters © Sage Etters',
+  //   year: '02015',
+  // },
+  // {
+  //   url: '/images/tyler-etters-adam-moore-hostel-earphoria.jpg',
+  //   title: 'Tyler Etters and Adam Moore at Hostel Earphoria © Riley Cavanaugh',
+  //   year: '02015',
+  // },
+  // {
+  //   url: '/images/tyler-etters-adam-moore-fox-river.jpg',
+  //   title: 'Tyler Etters and Adam Moore on the Fox River',
+  //   year: '02015',
+  // },
   {
     url: '/images/mayne-stage.jpg',
     title: 'Tyler Etters at Mayne Stage © Jordan Nelson',
     year: '02014',
   },
-  {
-    url: '/images/tyler-etters-adam-moore-paul-petrosyan-rakes-end.jpg',
-    title:
-      "Tyler Etters, Adam Moore, and Pasha Petrosyan at Rake's End © People Flavored",
-    year: '02013',
-  },
-  {
-    url: '/images/tyler-etters-magic-beach.jpg',
-    title: 'Tyler Etters at Magic Beach © Sage Etters',
-    year: '02013',
-  },
-  {
-    url: '/images/tyler-etters-and-arthur-zdrinc.jpg',
-    title: 'Tyler Etters & Arthur Zdrinc',
-    year: '02013',
-  },
-  {
-    url: '/images/tyler-etters-live.jpg',
-    title: 'Tyler Etters / Connectedness Locus at Dirtroom',
-    year: '02011',
-  },
+  // {
+  //   url: '/images/tyler-etters-adam-moore-paul-petrosyan-rakes-end.jpg',
+  //   title:
+  //     "Tyler Etters, Adam Moore, and Pasha Petrosyan at Rake's End © People Flavored",
+  //   year: '02013',
+  // },
+  // {
+  //   url: '/images/tyler-etters-magic-beach.jpg',
+  //   title: 'Tyler Etters at Magic Beach © Sage Etters',
+  //   year: '02013',
+  // },
+  // {
+  //   url: '/images/tyler-etters-and-arthur-zdrinc.jpg',
+  //   title: 'Tyler Etters & Arthur Zdrinc',
+  //   year: '02013',
+  // },
+  // {
+  //   url: '/images/tyler-etters-live.jpg',
+  //   title: 'Tyler Etters / Connectedness Locus at Dirtroom',
+  //   year: '02011',
+  // },
   {
     url: '/images/the-third-coast.jpg',
     title: 'The Third Coast',
     year: '02009',
   },
-  {
-    url: '/images/paul-petrosyan-tyler-etters-everything-comes-in-cycles-everything-fades-in-shades.jpg',
-    title:
-      'Pasha Petrosyan & Tyler Etters / Everything Comes in Cycles; Everything Fades in Shades',
-    year: '02009',
-  },
-  {
-    url: '/images/080827.jpg',
-    title: 'Mist',
-    year: '02008',
-  },
+  // {
+  //   url: '/images/paul-petrosyan-tyler-etters-everything-comes-in-cycles-everything-fades-in-shades.jpg',
+  //   title:
+  //     'Pasha Petrosyan & Tyler Etters / Everything Comes in Cycles; Everything Fades in Shades',
+  //   year: '02009',
+  // },
+  // {
+  //   url: '/images/080827.jpg',
+  //   title: 'Mist',
+  //   year: '02008',
+  // },
   {
     url: '/images/inocula.jpg',
     title: 'Inocula',
     year: '02008',
   },
-  {
-    url: '/images/healthy-outlets.jpg',
-    title: 'Inocula',
-    year: '02008',
-  },
-  {
-    url: '/images/l_6c4220799719533e3064b7eae3f9cf8f.jpg',
-    title: 'AJ Sugg & Tyler Etters / Amber Darkly',
-    year: '02007',
-  },
-  {
-    url: '/images/l_f844c99936ad05c519433a7048f6d5ed.jpg',
-    title: 'Inocula',
-    year: '02007',
-  },
+  // {
+  //   url: '/images/healthy-outlets.jpg',
+  //   title: 'Inocula',
+  //   year: '02008',
+  // },
+  // {
+  //   url: '/images/l_6c4220799719533e3064b7eae3f9cf8f.jpg',
+  //   title: 'AJ Sugg & Tyler Etters / Amber Darkly',
+  //   year: '02007',
+  // },
+  // {
+  //   url: '/images/l_f844c99936ad05c519433a7048f6d5ed.jpg',
+  //   title: 'Inocula',
+  //   year: '02007',
+  // },
   {
     url: '/images/tyler-etters-a-glass-reality.jpg',
     title: 'Film Still',
